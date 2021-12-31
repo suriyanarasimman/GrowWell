61b97fc232786db57b44cc795e0c739655f26519
suriyanarasimman <9444533098me@gmail.com>
2021-12-31 16:20:06 +0530

    Changes in Routing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { FooterComponent } from './footer/footer.component';
 
 
 const routes: Routes = [
-  { path:'',component:FooterComponent}
   ];
 
 @NgModule({
