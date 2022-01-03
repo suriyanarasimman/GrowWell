7df51a3ccf33f997a2e318168afa26bdf9301273
suriyanarasimman <9444533098me@gmail.com>
2022-01-03 19:13:49 +0530

    Data is reflecting correctly as per input

--- src/app/stockhistory/stockhistory.component.ts
+++ src/app/stockhistory/stockhistory.component.ts
@@ -3,6 +3,9 @@ import { Nametosymbol } from 'src/assets/nametosymbol';
 import { GeneralService } from '../services/general.service';
 import { FormControl } from '@angular/forms';
 import { StockDetails } from 'src/assets/stockdetails';
+import { Subscription } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-stockhistory',
@@ -11,12 +14,14 @@ import { StockDetails } from 'src/assets/stockdetails';
 })
 export class StockhistoryComponent implements OnInit {
 
+  private subscription: Subscription = new Subscription();
   public searchInput: String = '';
   public dateInput: String = '';
   public selected: String = "";
   public clicked: String;
   public searchResult: Array<Nametosymbol> = [];
   public stockdetails: StockDetails;
+  public Submitted1: String = "0";
 
   constructor(private generalService: GeneralService) { }
 
@@ -24,11 +29,17 @@ export class StockhistoryComponent implements OnInit {
   }
   datecalled(){
     console.log(this.dateInput)
-    this.generalService.fetchstockdetails(this.searchInput,this.dateInput).subscribe(
+    this.subscription.unsubscribe();
+    this.subscription =  this.generalService.fetchstockdetails(this.selected,this.dateInput).subscribe(
       (response) =>{
-this.stockdetails = response
+        this.stockdetails = response;
+        this.Submitted1 ="1";
+        console.log("Changes made")
+        console.log(this.stockdetails)
+        console.log(this.dateInput)
+        console.log(this.selected)
       })
-
+      
   }
 
   stockSelected(symbol:String){
