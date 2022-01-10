e1223dfd63d74768b56fad70df148cb7d15e932d
virtualmanu <26747363+virtualmanu@users.noreply.github.com>
2022-01-10 22:15:23 +0530

    api data in processed state as per our requirements

--- src/app/mutual-funds/filters/filters.component.ts
+++ src/app/mutual-funds/filters/filters.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Options } from '@angular-slider/ngx-slider';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import * as mf_data_module from "src/app/mutual-funds/mf_data.json";
+import { GeneralService } from 'src/app/services/general.service';
 
 @Component({
   selector: 'app-filters',
@@ -8,22 +11,110 @@ import { Options } from '@angular-slider/ngx-slider';
 })
 export class FiltersComponent implements OnInit {
 
-  value_min_invest: number = 1000;
-  value_cagr: number = 10;
+  value_min_invest: number = 200;
+  value_returns:number=10;
+
   options_min_invest: Options = {
     floor: 0,
-    ceil: 10000,
-    step:1000
+    ceil: 1000,
+    step:100
   };
-  options_cagr: Options = {
+
+  options_returns: Options = {
     floor: 0,
     ceil: 100,
-    step:1
+    step:10
   };
 
-  constructor() { }
+  form:FormGroup;
+
+  constructor(private gs:GeneralService) { }
 
   ngOnInit(): void {
-  }
+    this.form = new FormGroup({
+      equity: new FormControl(),
+      debt: new FormControl(),
+      flexi: new FormControl(),
 
+      tata_amc: new FormControl(),
+      icici_amc: new FormControl(),
+      ppfas_amc: new FormControl(),
+
+      minSlider: new FormControl(),
+      returnsSlider: new FormControl()
+    });
+  }
+  
+  fund_data={
+    "code":"",
+    "name":"",
+    "type":"",
+    "min_investment":"",
+    "3yr_returns":"",
+    "nav":"",
+    "last_update":""
+  }
+  
+  fund_data_array=[]
+
+  v_equity;
+  v_debt;
+  v_flexi;
+  v_icici_amc;
+  v_tata_amc;
+  v_ppfas_amc;
+  v_minSlider;
+  v_returnsSlider;
+
+  onSubmit(){
+    this.fund_data_array=[]
+
+
+    if(this.form.get("equity").value){
+      this.v_equity="equity";
+    }
+    if(this.form.get("debt").value){
+
+      this.v_debt="debt";
+    }
+    if(this.form.get("flexi").value){
+      this.v_flexi="flexi";
+
+    }
+    if(this.form.get("tata_amc").value){
+      this.v_tata_amc="tata";
+
+    }
+    if(this.form.get("icici_amc").value){
+      this.v_icici_amc="icici";
+    }
+    if(this.form.get("ppfas_amc").value){
+      this.v_ppfas_amc="ppfas";
+
+    }
+
+
+    mf_data_module.mf_data.forEach((obj)=>{
+      
+        this.gs.getFunds(obj.code).subscribe((response)=>{
+          this.fund_data={
+            "code":response.meta.scheme_code,
+            "name":response.meta.scheme_name,
+            "type":obj.type,
+            "min_investment":obj.min_investment,
+            "3yr_returns":obj['3yr_returns'],
+            "nav":response.data[0].nav,
+            "last_update":response.data[0].date
+          }
+    
+          // console.log(response);
+          // console.log(this.fund_data);
+
+          this.fund_data_array.push(this.fund_data);
+        });
+      
+    });
+    
+    console.log(this.fund_data_array);
+  }
 }
