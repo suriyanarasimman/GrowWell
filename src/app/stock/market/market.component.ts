9ec7a3473fe065d46804efd9e4b8887d4571f43c
suriyanarasimman <9444533098me@gmail.com>
2022-01-18 10:57:59 +0530

    Market backend working

--- src/app/stock/market/market.component.ts
+++ src/app/stock/market/market.component.ts
@@ -32,16 +32,56 @@ export class MarketComponent implements OnInit {
   qty=1;
   cost;
   flag=0;
+  sendPrice;
+  first=0;
+  searchResult;
   limitprice=1;
+  QuantityLimit;
+  sellProductAvailable=false;
   marketType: String= "Market";
   price:number =10;
-  username: string = localStorage.getItem('userName');;
+  userName: string = localStorage.getItem('userName');;
 
 
   constructor(private productService: GeneralService) {}
 
   ngOnInit() {
     this.setStockDetails("ADN");
+    this.fetchSeries()
+  }
+  datecalled(){
+    console.log(this.stockdetails.name)
+    
+    for (var i = 0; i < this.searchResult.length; i++){
+      if (this.searchResult[i].productID == this.stockdetails.name){
+        this.sellProductAvailable=true;
+        this.QuantityLimit = this.searchResult[i].quantity;
+      }
+    }
+  }
+  checkSell(){
+    this.datecalled()
+    if(!this.displayBuy){
+      if(!this.sellProductAvailable){
+        console.log("Not available")
+      }
+    }
+  }
+
+  fetchSeries(){
+    if(this.first === 0){
+    this.productService.productsOwnedByUser(this.userName).subscribe(
+      (res) => {
+        this.searchResult = res.filter(
+          (product) => product.productType === "STOCK"
+        );
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+    this.first =1
+    }
   }
 
   setStockDetails(name: String) {
@@ -92,7 +132,8 @@ export class MarketComponent implements OnInit {
     this.limitprice = this.limitprice + 1;
   }
   }
-  subPrice(){
+  subPrice()
+  {
     if(this.marketType !="Market"){
     if(this.flag==0)
     {this.limitprice = this.stockdetails.price -1;
@@ -102,5 +143,47 @@ export class MarketComponent implements OnInit {
     if(this.limitprice > 1)
     this.limitprice = this.limitprice - 1;
   }
+ }
+ onBuyFormSubmit() {
+  if(this.marketType == "Limit"){this.sendPrice = this.limitprice;}
+  if(this.marketType == "Market"){this.sendPrice =this.stockdetails.price;}
+  let buyOrderRequest = {
+    userName: this.userName,
+    productName: this.stockdetails.name,
+    productID: this.stockdetails.name,
+    productType: 'STOCK',
+    subcategory: 'STOCK',
+    buyPrice: this.sendPrice,
+    marketPrice: this.stockdetails.price,
+    quantity: this.qty,
+  };
+  this.productService.buyProduct(buyOrderRequest).subscribe(
+    (res) => {
+    }
+  );
+ }
+
+ onSellFormSubmit() {
+  if(this.marketType == "Limit"){this.sendPrice = this.limitprice;}
+  if(this.marketType == "Market"){this.sendPrice =this.stockdetails.price;}
+  let sellOrderRequest = {
+    userName: this.userName,
+    productName: this.stockdetails.name,
+    productID: this.stockdetails.name,
+    productType: 'STOCK',
+    subcategory: 'STOCK',
+    buyPrice: this.sendPrice,
+    marketPrice: this.stockdetails.price,
+    quantity: this.qty,
+  };
+  this.productService.sellProduct(sellOrderRequest).subscribe(
+    (res) => {
+      if (res.status === 'UPDATED') {
+        console.log("UPDATED")
+      } else {
+        console.log("Not Updated")
+      }
+    },
+  );
 }
 }
