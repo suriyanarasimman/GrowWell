2578a7476a142c28f67d38fc52136f65aebad19f
virtualmanu <26747363+virtualmanu@users.noreply.github.com>
2022-01-21 11:48:08 +0530

    mutual funds portfolio working

--- src/app/mutual-funds/portfolio/portfolio.component.ts
+++ src/app/mutual-funds/portfolio/portfolio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { GeneralService } from 'src/app/services/general.service';
 
 @Component({
   selector: 'app-portfolio',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
-  constructor() { }
+  userName:String;
+  funds:Array<Object>;
+
+  constructor(private gs:GeneralService, private cookieService:CookieService) {
+    if(this.cookieService.check("userName")) {
+      this.userName=this.cookieService.get("userName");
+    }
+    else {
+      this.userName=sessionStorage.getItem("userName");
+    };
+   }
 
   ngOnInit(): void {
+    this.gs.mutualFundsOwnedByUser(this.userName).subscribe((response)=>{
+      this.funds=response;
+    },
+    (error)=>{
+      console.log(error);
+    })
   }
 
 }
